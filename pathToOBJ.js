206a85431e2302a6422bad5aa6c52ac8826905bd
Shane Marks <info@shanemarks.co.za>
2014-02-19 15:24:52 +0200

    minor file cleanups

--- pathToOBJ.js
+++ pathToOBJ.js
@@ -5,40 +5,16 @@ function main()
 	if(!documents.length) return;
 	try
 	{
-         var imagewidth = activeDocument.width.as('px');
-         var imageheight = activeDocument.height.as('px');              //  image width and height used in normalization 
-		var Info = '';
-         var stream ="";
-		var workPath = app.activeDocument.pathItems.getByName("Path 1");
-        
-         
-         var count = 0;
-         var pointCache = 0;
-  
-        
-		for(var b =0; b < workPath.subPathItems.length; b++)
-		{
-            
-         var verts = "";
-         var normals = "";
-         var uvs = "";
-         var  triangles = "f ";
-         var cache = workPath.subPathItems[b].pathPoints.length;
+      
+        if ( saveFile(PathsToObj (app.activeDocument.pathItems.getByName ( "Work Path" ))))
+        {
+            alert ("File saved Successfully");
+        }
 
-            for (var p = 0; p < workPath.subPathItems[b].pathPoints.length; p++)
-            {
-              verts += "v " +(workPath.subPathItems[b].pathPoints[p].anchor[0]) + " " + (imageheight - workPath.subPathItems[b].pathPoints[p].anchor[1]) + " 0\r" ;              
-              normals += "vn  0 0 1\r"  ;
-              uvs += "vt "  + (workPath.subPathItems[b].pathPoints[p].anchor[0] / imagewidth ) + " " + (1 - (workPath.subPathItems[b].pathPoints[p].anchor[1] / imageheight)) + "\r";
-              count = (cache - p + pointCache);
-              triangles += count+"/"+count +"/"+count +" ";
-              
-            }
-                pointCache +=cache;
-               stream += verts + normals + uvs + triangles + "\r";
-	    }
- 
-        saveFile(stream);
+        else
+        {
+            alert ("File failed to save");
+        }
 
 	}
 
@@ -50,12 +26,48 @@ function main()
 }
 
 
-function saveFile(txt)
+
+// This function takes in a given set of paths and converts the information to a poly mesh (OBJ format). Uv's are also generated
+function PathsToObj (workPath)
+{
+
+        var imagewidth = activeDocument.width.as('px');                
+        var imageheight = activeDocument.height.as('px');                   //image width and height used in normalization 
+        var stream =""; 
+        var count = 0;                                                     
+        var pointCache = 0;                                                 //stores cache of amount of points counted, used in OBJ generation
+  
+        
+        for(var b =0; b < workPath.subPathItems.length; b++)                //loop though each path segment
+        {
+            
+            var verts = "";
+            var normals = "";
+            var uvs = "";
+            var triangles = "f ";
+            var cache = workPath.subPathItems[b].pathPoints.length;
+
+            for (var p = 0; p < workPath.subPathItems[b].pathPoints.length; p++) //loop through each point in path
+            {
+                verts += "v " +(workPath.subPathItems[b].pathPoints[p].anchor[0]) + " " + (imageheight - workPath.subPathItems[b].pathPoints[p].anchor[1]) + " 0\r" ;              
+                normals += "vn  0 0 1\r"  ;
+                uvs += "vt "  + (workPath.subPathItems[b].pathPoints[p].anchor[0] / imagewidth ) + " " + (1 - (workPath.subPathItems[b].pathPoints[p].anchor[1] / imageheight)) + "\r";
+                count = (cache - p + pointCache);
+                triangles += count+"/"+count +"/"+count +" ";
+            }
+            pointCache +=cache;
+            stream += verts + normals + uvs + triangles + "\r";
+        }
+ 
+    return stream;
+}
+
+function saveFile(txt) //Savee OBJ to disk. this requires the PSD to already be saved. OBJ file gets saved next to PSD.
 {
     var Name = app.activeDocument.name.replace(/\.[^\.]+$/, '');
     var Ext = decodeURI(app.activeDocument.name).replace(/^.*\./,'');
     if (Ext.toLowerCase() != 'psd')
-        return;
+        return false;
 
     var Path = app.activeDocument.path;
     var saveFile = File(Path + "/" + Name +".obj");
@@ -67,5 +79,5 @@ function saveFile(txt)
     saveFile.open("e", "TEXT", "????");
     saveFile.writeln(txt);
     saveFile.close();
-    alert ("File Saved To: " + saveFile)
+    return true;
 }
